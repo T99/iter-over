34c05b83bef3f05d756e0aa33205445aa0291a13
Trevor Sears <trevorsears.main@gmail.com>
2019-07-21 20:38:03 -0400

    Pushed more compiled files.

--- js/empty-iterator.js
+++ js/empty-iterator.js
@@ -1,19 +1,2 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const abstract_iterator_1 = require("./abstract-iterator");
-class EmptyIterator extends abstract_iterator_1.AbstractIterator {
-    hasNext() {
-        return false;
-    }
-    next() {
-        return undefined;
-    }
-    remove() {
-        return undefined;
-    }
-    reset() {
-        return;
-    }
-}
-exports.EmptyIterator = EmptyIterator;
+"use strict";Object.defineProperty(exports,"__esModule",{value:!0});const abstract_iterator_1=require("./abstract-iterator");class EmptyIterator extends abstract_iterator_1.AbstractIterator{hasNext(){return!1}next(){}remove(){}reset(){}}exports.EmptyIterator=EmptyIterator;
 //# sourceMappingURL=empty-iterator.js.map
